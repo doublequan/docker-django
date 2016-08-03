dd1c1d36d8941feef7edca448c003f301a17d949
doublequan <billdqu@gmail.com>
2016-08-03 19:02:47 +0800

    update

--- web/web_scrapy/scrapy_repeat.sh
+++ web/web_scrapy/scrapy_repeat.sh
@@ -8,8 +8,8 @@ time=$(date "+%Y-%m-%d %H:%M")
 echo "${time}: start run crawler 1p3" >> /code/logs/shell.log
 
 # cd /code/web_scrapy
-# ls >> /code/crontab.log
 /usr/local/bin/scrapy crawl 1p3
 
-echo "finish crawling" >> /code/logs/shell.log
+time=$(date "+%Y-%m-%d %H:%M")
+echo "${time}: finish crawling" >> /code/logs/shell.log
 
